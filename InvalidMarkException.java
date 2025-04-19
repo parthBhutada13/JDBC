c6749582244a420c288b04561e7ca4e247aae5a0
parth bhutada <parthbhutada13@gmail.com>
2025-04-19 19:40:16 +0530

    Create InvalidMarkException.java

---
+++ InvalidMarkException.java
@@ -0,0 +1,12 @@
+//InvalidMarkException
+package student_database;
+
+public class InvalidMarkException extends Exception {
+    public InvalidMarkException() {
+        super("Marks must be between 0 and 100");
+    }
+
+    public InvalidMarkException(double marks) {
+        super("Invalid marks value: " + marks + ". Must be between 0-100");
+    }
+}
